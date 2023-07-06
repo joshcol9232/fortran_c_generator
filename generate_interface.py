acafe97b53135a7500c2cc17c863bd66df964628
Josh Colclough <colcloughj2@cardiff.ac.uk>
2023-07-06 22:53:42 +0100

    Lay-out structure

--- generate_interface.py
+++ generate_interface.py
@@ -1,5 +1,62 @@
-from open_fortran_parser import parse
+import open_fortran_parser as ofp
+import xml.etree.ElementTree as ET
+
+class Argument:
+    def __init__(self, name, ftype):
+        self.name = name
+        self.ftype = ftype
+
+class Subroutine:
+    # Subroutine name, subroutine arguments
+    def __init__(self, name, *args):
+        self.name = name
+        self.args = list(args)
+
+    @classmethod
+    def from_root_xml_node(xml_node):
+        
+
+        return Subroutine()
+
+
+def parse_subroutines(tree):
+    print("Parsing subroutines...")
+
+    subroutines = tree.findall("subroutine")
+    for subroutine in subroutines:
+        name = subroutine.attrib["name"]
+        print("Found subroutine:", name)
+
+    return []
+
+
+def parse_functions(tree):
+    return []
+
+def make_f_interface_top(module_name):
+    return ""
+
+def make_f_interface_subroutines(subroutines):
+    return ""
+
+def make_f_interface_functions(functions):
+    return ""
+
 
 if __name__ == "__main__":
-    xml = parse("example.F90", verbosity=0)
-    print(type(xml))
+    tree = ofp.parse("example.F90")
+    # print(ET.dump(tree))
+    # TODO: Check there is only one? Is this needed?
+    file = tree.find("file")
+
+    subroutines = parse_subroutines(file)
+    functions = parse_functions(file)
+
+    # TODO: Parse from original module
+    module_name = "example_interface"
+
+    f_interface = ""
+    f_interface += make_f_interface_top(module_name) 
+    f_interface += make_f_interface_subroutines(subroutines)
+    f_interface += make_f_interface_functions(functions)
+
